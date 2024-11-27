9bce06e2d789f99f246baa8e168721558e66f5f7
Dima Steklar <dsteklar@cisco.com>
2024-11-27 11:26:27 +0200

    modify

--- api_views/test_func.py
+++ api_views/test_func.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def mock_bad_implementation(r: str, p: int, t: str):
-    a = f"https://api.github.com/repos/{r}/pulls/{p}"
-    h = {"Authorization": "Bearer " + t}
+def mock_bad_implementation(r: str, p: int, g: str):
+    a = f"https://api123.github.com/repos/{r}/pulls/{p}"
+    h = {"Authorization": "Bearer " + g}
     x = requests.get(a, headers=h)
     return x.json()  # This assumes everything always works perfectly
