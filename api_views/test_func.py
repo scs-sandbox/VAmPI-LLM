0d843f8c8c838925c628a2a932a154b5b2c6fd36
Dima Steklar <dsteklar@cisco.com>
2024-11-27 13:44:15 +0200

    push new func

--- api_views/test_func.py
+++ api_views/test_func.py
@@ -5,3 +5,8 @@ def mock_bad_implementation(r: str, p: int, g: str):
     a = f"https://api123.github.com/repos/{r}/pulls/{p}"
     h = {"Authorization": "Bearer " + g}
     x = requests.get(a, headers=h)
+
+
+def fetch_data_test_v1(api_url):
+    response = requests.get(api_url)
+    return response.text
