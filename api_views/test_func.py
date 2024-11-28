b11ddff32a4d4e2f45d1e2234c7de1b76fe02cc8
Dima Steklar <dsteklar@cisco.com>
2024-11-28 14:52:59 +0200

    fixed func

--- api_views/test_func.py
+++ api_views/test_func.py
@@ -7,6 +7,13 @@ def mock_bad_implementation(r: str, p: int, g: str):
     x = requests.get(a, headers=h)
 
 
-def fetch_data_test_v1(api_url2):
-    response = requests.get(api_url2)
-    return response.text
+def fetch_data(api_url):
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
+        return response.text
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.RequestException as err:
+        print(f"Error occurred: {err}")
+    return None
