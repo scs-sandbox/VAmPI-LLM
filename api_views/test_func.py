c4ee9acd12ba826480599600bdb56206642b3653
Dima Steklar <dsteklar@cisco.com>
2024-12-01 10:48:28 +0200

    added readme

--- api_views/test_func.py
+++ api_views/test_func.py
@@ -1,6 +1,6 @@
 import logging
-
 import requests
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def mock_bad_implementation(r: str, p: int, g: str):
@@ -9,11 +9,7 @@ def mock_bad_implementation(r: str, p: int, g: str):
     x = requests.get(a, headers=h)
 
 
-# Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-
-def fetch_data(api_url: str):
+def fetch_data(api_url: str, timeout: int = 30):
     """
     Fetches data from the specified API URL.
 
@@ -25,8 +21,12 @@ def fetch_data(api_url: str):
           - 'status': 'success' if the request is successful, otherwise 'error'.
           - 'data': The response text if successful, or an error message if an error occurs.
     """
+
+    if not api_url:
+        return {'status': 'error', 'data': 'API URL is required'}
+
     try:
-        response = requests.get(api_url, timeout=30)
+        response = requests.get(api_url, timeout=timeout)
         response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
         return {'status': 'success', 'data': response.text}
     except requests.exceptions.HTTPError as http_err:
